3dedda5a06ed9c75843b5c924b09d246adc0bb45
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-03-02 21:45:57 +0000

    add search bar, and hidde booking to activity owner

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,14 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = Activity.all
+    if params[:query].present?
+      sql_query = <<~SQL
+        activities.location ILIKE :query
+        OR activities.description ILIKE :query
+      SQL
+      @activities = Activity.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @activities = Activity.all
+    end
   end
 
   def show
