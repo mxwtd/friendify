52938b80c57c038b3944d066edeb8a78baf4b5c7
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-03-03 08:53:50 +0000

    add name and age fields to the sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
       protected
 
       def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:user_photo])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:user_photo])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:user_photo, :name, :age])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:user_photo, :name, :age])
       end
 end
