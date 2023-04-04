a02837b2cf910ae0fa539d85f17b32532ce2214c
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-04-04 21:32:54 +0200

    add map to the app

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,37 +1,39 @@
 import { Controller } from "@hotwired/stimulus"
 import { Loader } from "@googlemaps/js-api-loader";
-import { container } from "webpack";
+// import { container } from "webpack";
 
 // Connects to data-controller="map"
 export default class extends Controller {
+  static values = { api: String }
+  
   static targets = ["map"]
-  static values = { apiKey: String }
 
   connect() {
-    console.log("Listing Map connecteed!");
+    console.log("Listing Map connected");
     console.log(this.mapTarget);
+    console.log(this.apiValue);
+    
+    const loader = new Loader({
+      apiKey: this.apiValue,
+      version: "weekly",
+      libraries: ["places"],
+    });
 
-    // const loader = new Loader({
-    //   apiKey: "AIzaSyDeFR90LQKYXflRcL-SMRQCsQrXIH6EvsY",
-    //   // apiKey: "<%=ENV['GOOGLE_MAPS_API']%>",
-    //   version: "weekly",
-    //   libraries: ["places"],
-    // });
-
-    // loader.load().then(() => {
-    //   this.initMap();
-    // });
-    this.initMap();
+    loader.load().then(() => {
+      this.initMap();
+    });
   }
 
   initMap() {
-    // Call the initMap function here
-    console.log(this.apiKeyValue);
     console.log("init Map function is working");
-    let uluru = {lat: -25.344, lng: 132.036};
-    let map = new google.maps.Map(
-      this.mapTarget,
-      {zoom: 4, center: uluru}
-    )
+    var map = new google.maps.Map(this.mapTarget, {
+      zoom: 5,
+      center: {lat: 42.402900, lng: -122.885240}
+    });
+
+    const marker = new google.maps.Marker({
+      position: {lat: 42.402900, lng: -122.885240},
+      map: map,
+    });
   }
 }
